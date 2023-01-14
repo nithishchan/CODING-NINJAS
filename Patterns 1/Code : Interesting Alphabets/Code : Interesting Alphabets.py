c7a28130f528af24a9becc9199073b82def509f9
Nithish Chandra <83600538+nithishchan@users.noreply.github.com>
2023-01-14 13:20:42 +0530

    Create Code : Interesting Alphabets.py

---
+++ Patterns 1/Code : Interesting Alphabets/Code : Interesting Alphabets.py
@@ -0,0 +1,43 @@
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+
+l=['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+num=int(input())
+c=num
+for i in range(1,num+1):
+    for j in range(0,i):
+        print(l[c+j-1],end="")
+    c = c - 1
+    print()
