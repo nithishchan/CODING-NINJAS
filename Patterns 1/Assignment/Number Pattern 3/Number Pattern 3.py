0f73b257d11043cc0c99d60e5cdd7e0b27ada94b
Nithish Chandra <83600538+nithishchan@users.noreply.github.com>
2023-01-14 13:22:40 +0530

    Create Number Pattern 3.py

---
+++ Patterns 1/Assignment/Number Pattern 3/Number Pattern 3.py
@@ -0,0 +1,32 @@
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+11
+121
+1221
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Sample Input :
+5
+Sample Output :
+1
+11
+121
+1221
+12221
+
+
+N = int(input())
+for i in range(1, N+1):
+    for j in range(0, i):
+        x = i-1
+        if x == 0:
+            print(1, end="")
+        else:
+            if x == j or j == 0:
+                print(1, end="")
+            else:
+                print(2, end="")
+    print("")
