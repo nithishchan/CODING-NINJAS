446a9b4f8dc63a4357d0915387ebb1c0c269ee58
Nithish Chandra <83600538+nithishchan@users.noreply.github.com>
2023-01-21 18:21:38 +0530

    Create Maximum Frequency.py

---
+++ DATA STRUCTURES & ALGORITHMS IN PYTHON/Dictionaries/Maximum Frequency/Maximum Frequency.py
@@ -0,0 +1,53 @@
+You are given an array of integers that contain numbers in random order. Write a program to find and return the number which occurs the maximum times in the given input.
+If two or more elements contend for the maximum frequency, return the element which occurs in the array first.
+Input Format:
+The first line of input contains an integer, that denotes the value of the size of the array. Let us denote it with the symbol N.
+The following line contains N space separated integers, that denote the value of the elements of the array.
+Output Format :
+The first and only line of output contains most frequent element in the given array.
+Constraints:
+0 <= N <= 10^8
+Time Limit: 1 sec
+Sample Input 1 :
+13
+2 12 2 11 12 2 1 2 2 11 12 2 6 
+Sample Output 1 :
+2
+Sample Input 2 :
+3
+1 4 5
+Sample Output 2 :
+1
+
+
+
+
+# Read input as sepcified in the question
+# Print output as specified in the question
+from sys import stdin
+
+def maxfreq(arr):
+    d={}
+    for num in arr:
+        if num in d:
+            d[num]+=1
+        else:
+            d[num]=1
+    ans=arr[0]
+    for num in arr:
+        if d[num]>d[ans]:
+            ans=num
+    return ans
+
+
+def takeInput():
+    #To take fast I/O
+    n = int(stdin.readline().strip())
+    if n == 0:
+        return list(), 0
+    arr = list(map(int, stdin.readline().strip().split()))
+    return arr, n
+
+
+arr, n = takeInput()
+print(maxfreq(arr))
