c4d7ccc7fdecf75fabedd37db16d1f12a10cf3f6
Nithish Chandra <83600538+nithishchan@users.noreply.github.com>
2023-02-04 15:13:17 +0530

    Create Check Max-Heap.py

---
+++ DATA STRUCTURES & ALGORITHMS IN PYTHON/Priority Queues - 2/Check Max-Heap/Check Max-Heap.py
@@ -0,0 +1,16 @@
+def checkMaxHeap(lst):
+    n = len(lst)
+    for i in range(n):
+        left = (2*i) + 1
+        right = left + 1
+        if (left<n) and lst[left]>lst[i]:
+            return False
+        if (right<n) and lst[right]>lst[i]:
+            return False
+    return True
+    pass
+
+# Main Code
+n=int(input())
+lst=list(int(i) for i in input().strip().split(' '))
+print('true') if checkMaxHeap(lst) else print('false')
